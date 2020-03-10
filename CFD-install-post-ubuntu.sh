99a5db2036e0cdbd6b4a057b7efe92c5e87896dc
JulienReveillon <Julien.Reveillon@coria.fr>
2020-03-10 12:38:03 +0100

    work in progress

--- CFD-install-post-ubuntu.sh
+++ CFD-install-post-ubuntu.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-# Automatically generated script by
-# vagrantbox/doc/src/vagrant/src-vagrant/deb2sh.py
-# where vagrantbox is the directory arising from
-# git clone git@github.com:hplgit/vagrantbox.git
-
-# The script is based on packages listed in LinuxPkg.txt.
 
 set -x  # make sure each command is printed in the terminal
 
@@ -17,12 +11,13 @@ function apt_install {
 }
 
 function pip_install {
-  sudo -H pip install --upgrade "$@"
+  sudo -H pip3 install --upgrade "$@"
   if [ $? -ne 0 ]; then
     echo "could not install $p - abort"
     exit 1
   fi
 }
+
 
 sudo apt-get update --fix-missing
 
@@ -30,16 +25,33 @@ sudo apt-get update --fix-missing
 # cd
 if [ ! -d srclib ]; then mkdir srclib; fi
 
-# JR Editors
-apt_install emacs
-apt_install emacs24-lucid
-apt_install python-mode
-apt_install gedit
+
+##### Misc
+apt_install wget
+
+
+##### Text Editors
+# atom editor
+sudo add-apt-repository ppa:webupd8team/atom -y
+sudo apt-get update
+apt_install atom
+# vim
 apt_install vim
 apt_install ispell
+# latex editor
 apt_install texstudio
+# edition
+apt_install meld
 
-# Compilers
+
+##### Version control
+apt_install subversion
+apt_install mercurial
+apt_install cvs
+apt_install git
+apt_install gitk
+
+# Compilers and compilation
 apt_install gcc
 apt_install g++
 apt_install gawk
@@ -50,49 +62,45 @@ apt_install automake
 apt_install autotools-dev
 apt_install cmake
 
+
+
 # Numerical libraries
-apt_install libatlas-base-dev
-apt_install libsuitesparse-dev
+#apt_install libatlas-base-dev
+#apt_install libsuitesparse-dev
 
 # JR Software development and OF compilation necessary
-apt_install git-core
-apt_install build-essential
-apt_install binutils-dev
-apt_install cmake
-apt_install flex
-apt_install bison
-apt_install meld
-apt_install zlib1g-dev
-apt_install qt4-dev-tools
-apt_install libqt4-dev
-apt_install libqtwebkit-dev
-apt_install libreadline-dev
-apt_install libncurses-dev
-apt_install libxt-dev
-apt_install libopenmpi-dev
-apt_install openmpi-bin
-apt_install libboost-system-dev
-apt_install libboost-thread-dev
-apt_install libgmp-dev
-apt_install libmpfr-dev
-apt_install libcgal-dev
-apt_install libglu1-mesa-dev
-apt_install libqt4-opengl-dev
-apt_install hdf5-helpers
-apt_install hdf5-tools
-apt_install hdfview
-apt_install libhdf5-10
-apt_install libhdf5-cpp-11
-apt_install libhdf5-dev
-apt_install wget
+# most of them are already installed
+# apt_install build-essential
+# apt_install binutils-dev
+# apt_install cmake
+# apt_install flex
+# apt_install bison
+# apt_install zlib1g-dev
+# apt_install qt4-dev-tools
+# apt_install libqt4-dev
+# apt_install libqtwebkit-dev
+# apt_install libreadline-dev
+# apt_install libncurses-dev
+# apt_install libxt-dev
+# apt_install libopenmpi-dev
+# apt_install openmpi-bin
+# apt_install libboost-system-dev
+# apt_install libboost-thread-dev
+# apt_install libgmp-dev
+# apt_install libmpfr-dev
+# apt_install libcgal-dev
+# apt_install libglu1-mesa-dev
+# apt_install libqt4-opengl-dev
+# apt_install hdf5-helpers
+# apt_install hdf5-tools
+# apt_install hdfview
+# apt_install libhdf5-10
+# apt_install libhdf5-cpp-11
+# apt_install libhdf5-dev
 
 
-# JR Version control systems
-apt_install subversion
-apt_install mercurial
-apt_install cvs
-apt_install git
-apt_install gitk
+
+
 
 #
 #  JR Python
@@ -100,14 +108,12 @@ apt_install gitk
 
 # Matplotlib requires libfreetype-dev libpng-dev
 # (otherwise pip install matplotlib does not work)
-apt_install libfreetype6-dev
-apt_install libpng-dev
-apt_install idle
-apt_install spyder
+#apt_install libfreetype6-dev
+#apt_install libpng-dev
+#apt_install idle
 
-# Python3 
+# Python3
 apt_install python3-pip
-apt_install python3-dev
 apt_install python3-opengl
 # upgrade pip3
 sudo -H pip3 install --upgrade pip
@@ -127,6 +133,8 @@ pip_install pytest
 pip_install flake8
 pip_install numpydoc
 pip_install pygame
+pip_install xkcd
+
 #
 # JR plot, CAD, mesh softwares
 #
@@ -143,9 +151,9 @@ apt_install liblualib50-dev
 
 
 # Plotting and visualization programs
-apt_install grace
-apt_install dx
-apt_install dx-doc
+#apt_install grace
+#apt_install dx
+#apt_install dx-doc
 apt_install tcl-vtk
 apt_install python-vtk
 apt_install libvtk5-dev
@@ -189,51 +197,53 @@ apt_install libav-tools
 apt_install language-pack-fr-base
 apt_install language-pack-fr
 
-# Download source code and install in srclib subdirectory
-
-#Works only with pip2
-#pip install -e git+https://github.com/hplgit/scitools.git#egg=scitools
-#pip install -e git+https://github.com/hplgit/odespy.git#egg=odespy
 
 
 # OPENFOAM
 
-# Install OpenFOAM code in OpenFOAM
-# cd
-if [ ! -d OpenFOAM ]; then mkdir OpenFOAM; fi
+# OpenFoam fondation package
+sudo sh -c "wget -O - http://dl.openfoam.org/gpg.key | apt-key add -"
+sudo add-apt-repository http://dl.openfoam.org/ubuntu
+sudo apt-get update
 
-cd OpenFOAM
-wget "http://download.openfoam.org/source/4-1" -O OpenFOAM-4.1.tgz
-wget "http://download.openfoam.org/third-party/4-1" -O ThirdParty-4.1.tgz
-tar -xzf OpenFOAM-4.1.tgz
-tar -xzf ThirdParty-4.1.tgz
-mv OpenFOAM-4.x-version-4.1 OpenFOAM-4.1
-mv ThirdParty-4.x-version-4.1 ThirdParty-4.1
-ln -s /usr/bin/mpicc.openmpi OpenFOAM-4.1/bin/mpicc
-ln -s /usr/bin/mpirun.openmpi OpenFOAM-4.1/bin/mpirun
-source $HOME/OpenFOAM/OpenFOAM-4.1/etc/bashrc WM_LABEL_SIZE=64 FOAMY_HEX_MESH=yes
-echo "alias of41='source \$HOME/OpenFOAM/OpenFOAM-4.1/etc/bashrc $FOAM_SETTINGS'" >> $HOME/.bashrc
+# Install OpenFOAM - ESI version
+if [ ! -d $HOME/OpenFOAM ]; then mkdir $HOME/OpenFOAM; fi
+cd $HOME/OpenFOAM
+wget "https://sourceforge.net/projects/openfoam/files/v1912/OpenFOAM-v1912.tgz" -O OpenFOAM-v1912.tgz
+wget "https://sourceforge.net/projects/openfoam/files/v1912/ThirdParty-v1912.tgz" -O ThirdParty-v1912.tgz
+tar -xzf OpenFOAM-v1912.tgz
+tar -xzf ThirdParty-v1912.tgz
+source OpenFOAM-v1912/etc/bashrc
 cd $WM_THIRD_PARTY_DIR
-export QT_SELECT=qt4
-./makeParaView -python -mpi -python-lib /usr/lib/x86_64-linux-gnu/libpython2.7.so.1.0 > log.makePV 2>&1
-wmRefresh
-cd $WM_PROJECT_DIR
-export QT_SELECT=qt4
-./Allwmake > log.make 2>&1
-cd ..
-rm OpenFOAM-4.1.tgz
-rm ThirdParty-4.1.tgz
+./makeParaview
 
-# Clean up
-sudo mv -f src/* srclib
-sudo rm -rf src build
-sudo find srclib -name build -exec rm -rf {} \;
-cd
-sudo rm -rf .matplotlib
-mkdir .matplotlib
-sudo apt-get clean
-sudo apt-get autoclean
-sudo apt-get -y autoremove
+
+
+# ln -s /usr/bin/mpicc.openmpi OpenFOAM-4.1/bin/mpicc
+# ln -s /usr/bin/mpirun.openmpi OpenFOAM-4.1/bin/mpirun
+# source $HOME/OpenFOAM/OpenFOAM-4.1/etc/bashrc WM_LABEL_SIZE=64 FOAMY_HEX_MESH=yes
+# echo "alias of41='source \$HOME/OpenFOAM/OpenFOAM-4.1/etc/bashrc $FOAM_SETTINGS'" >> $HOME/.bashrc
+# cd $WM_THIRD_PARTY_DIR
+# export QT_SELECT=qt4
+# ./makeParaView -python -mpi -python-lib /usr/lib/x86_64-linux-gnu/libpython2.7.so.1.0 > log.makePV 2>&1
+# wmRefresh
+# cd $WM_PROJECT_DIR
+# export QT_SELECT=qt4
+# ./Allwmake > log.make 2>&1
+# cd ..
+# rm OpenFOAM-4.1.tgz
+# rm ThirdParty-4.1.tgz
+#
+# # Clean up
+# sudo mv -f src/* srclib
+# sudo rm -rf src build
+# sudo find srclib -name build -exec rm -rf {} \;
+# cd
+# sudo rm -rf .matplotlib
+# mkdir .matplotlib
+# sudo apt-get clean
+# sudo apt-get autoclean
+# sudo apt-get -y autoremove
 
 
 
@@ -243,4 +253,3 @@ sudo apt-get -y autoremove
 
 
 echo "Everything is successfully installed!"
-
