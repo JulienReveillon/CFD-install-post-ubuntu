276dd088547442d5bf899e9f94b8867a58365c3b
Julien Reveillon <julienreveillon@smilla.insa-rouen.fr>
2020-03-10 10:56:09 +0100

    original sh

--- CFD-install-post-ubuntu.sh
+++ CFD-install-post-ubuntu.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-
-
-
-
-
-
-sudo apt-get -y
-sudo apt-get -y instal:
-#!/usr/bin/env python
 # Automatically generated script by
 # vagrantbox/doc/src/vagrant/src-vagrant/deb2sh.py
 # where vagrantbox is the directory arising from
@@ -15,97 +6,93 @@ sudo apt-get -y instal:
 
 # The script is based on packages listed in LinuxPkg.txt.
 
-logfile = 'tmp_output.log'  # store all output of all operating system commands
-f = open(logfile, 'w'); f.close()  # touch logfile so it can be appended
+set -x  # make sure each command is printed in the terminal
 
-import subprocess, sys
+function apt_install {
+  sudo apt-get -y install $1
+  if [ $? -ne 0 ]; then
+    echo "could not install $1 - abort"
+    exit 1
+  fi
+}
 
-def system(cmd):
-    """Run system command cmd."""
-    print cmd
-    try:
-        output = subprocess.check_output(cmd, shell=True,
-                                         stderr=subprocess.STDOUT)
-    except subprocess.CalledProcessError as e:
-        print 'Command\n  %s\nfailed.' % cmd
-        print 'Return code:', e.returncode
-        print e.output
-        sys.exit(1)
-    print output
-    f = open(logfile, 'a'); f.write(output); f.close()
+function pip_install {
+  sudo -H pip install --upgrade "$@"
+  if [ $? -ne 0 ]; then
+    echo "could not install $p - abort"
+    exit 1
+  fi
+}
 
-system('sudo apt-get update --fix-missing')
+sudo apt-get update --fix-missing
+
 # Install downloaded source code in ~/srclib
 # cd
-
-cmd = """
 if [ ! -d srclib ]; then mkdir srclib; fi
-# JR Editors
 
-"""
-system(cmd)
-system('sudo apt-get -y install emacs')
-system('sudo apt-get -y install emacs24-lucid')
-system('sudo apt-get -y install python-mode')
-system('sudo apt-get -y install gedit')
-system('sudo apt-get -y install vim')
-system('sudo apt-get -y install ispell')
-system('sudo apt-get -y install texstudio')
+# JR Editors
+apt_install emacs
+apt_install emacs24-lucid
+apt_install python-mode
+apt_install gedit
+apt_install vim
+apt_install ispell
+apt_install texstudio
 
 # Compilers
-system('sudo apt-get -y install gcc')
-system('sudo apt-get -y install g++')
-system('sudo apt-get -y install gawk')
-system('sudo apt-get -y install f2c')
-system('sudo apt-get -y install gfortran')
-system('sudo apt-get -y install autoconf')
-system('sudo apt-get -y install automake')
-system('sudo apt-get -y install autotools-dev')
-system('sudo apt-get -y install cmake')
+apt_install gcc
+apt_install g++
+apt_install gawk
+apt_install f2c
+apt_install gfortran
+apt_install autoconf
+apt_install automake
+apt_install autotools-dev
+apt_install cmake
 
 # Numerical libraries
-system('sudo apt-get -y install libatlas-base-dev')
-system('sudo apt-get -y install libsuitesparse-dev')
+apt_install libatlas-base-dev
+apt_install libsuitesparse-dev
 
 # JR Software development and OF compilation necessary
-system('sudo apt-get -y install git-core')
-system('sudo apt-get -y install build-essential')
-system('sudo apt-get -y install binutils-dev')
-system('sudo apt-get -y install cmake')
-system('sudo apt-get -y install flex')
-system('sudo apt-get -y install bison')
-system('sudo apt-get -y install meld')
-system('sudo apt-get -y install zlib1g-dev')
-system('sudo apt-get -y install qt4-dev-tools')
-system('sudo apt-get -y install libqt4-dev')
-system('sudo apt-get -y install libqtwebkit-dev')
-system('sudo apt-get -y install libreadline-dev')
-system('sudo apt-get -y install libncurses-dev')
-system('sudo apt-get -y install libxt-dev')
-system('sudo apt-get -y install libopenmpi-dev')
-system('sudo apt-get -y install openmpi-bin')
-system('sudo apt-get -y install libboost-system-dev')
-system('sudo apt-get -y install libboost-thread-dev')
-system('sudo apt-get -y install libgmp-dev')
-system('sudo apt-get -y install libmpfr-dev')
-system('sudo apt-get -y install libcgal-dev')
-system('sudo apt-get -y install libglu1-mesa-dev')
-system('sudo apt-get -y install libqt4-opengl-dev')
-system('sudo apt-get -y install hdf5-helpers')
-system('sudo apt-get -y install hdf5-tools')
-system('sudo apt-get -y install hdfview')
-system('sudo apt-get -y install libhdf5-10')
-system('sudo apt-get -y install libhdf5-cpp-11')
-system('sudo apt-get -y install libhdf5-dev')
-system('sudo apt-get -y install wget')
+apt_install git-core
+apt_install build-essential
+apt_install binutils-dev
+apt_install cmake
+apt_install flex
+apt_install bison
+apt_install meld
+apt_install zlib1g-dev
+apt_install qt4-dev-tools
+apt_install libqt4-dev
+apt_install libqtwebkit-dev
+apt_install libreadline-dev
+apt_install libncurses-dev
+apt_install libxt-dev
+apt_install libopenmpi-dev
+apt_install openmpi-bin
+apt_install libboost-system-dev
+apt_install libboost-thread-dev
+apt_install libgmp-dev
+apt_install libmpfr-dev
+apt_install libcgal-dev
+apt_install libglu1-mesa-dev
+apt_install libqt4-opengl-dev
+apt_install hdf5-helpers
+apt_install hdf5-tools
+apt_install hdfview
+apt_install libhdf5-10
+apt_install libhdf5-cpp-11
+apt_install libhdf5-dev
+apt_install wget
 
 
 # JR Version control systems
-system('sudo apt-get -y install subversion')
-system('sudo apt-get -y install mercurial')
-system('sudo apt-get -y install cvs')
-system('sudo apt-get -y install git')
-system('sudo apt-get -y install gitk')
+apt_install subversion
+apt_install mercurial
+apt_install cvs
+apt_install git
+apt_install gitk
 
 #
 #  JR Python
@@ -113,98 +100,94 @@ system('sudo apt-get -y install gitk')
 
 # Matplotlib requires libfreetype-dev libpng-dev
 # (otherwise pip install matplotlib does not work)
-system('sudo apt-get -y install libfreetype6-dev')
-system('sudo apt-get -y install libpng-dev')
-system('sudo apt-get -y install idle')
-system('sudo apt-get -y install spyder')
+apt_install libfreetype6-dev
+apt_install libpng-dev
+apt_install idle
+apt_install spyder
 
 # Python3 
-system('sudo apt-get -y install python3-pip')
-system('sudo apt-get -y install python3-dev')
-system('sudo apt-get -y install python3-opengl')
+apt_install python3-pip
+apt_install python3-dev
+apt_install python3-opengl
 # upgrade pip3
-
-cmd = """
 sudo -H pip3 install --upgrade pip
-#pip  install
 
-"""
-system(cmd)
-system('sudo pip install setuptools')
-system('sudo pip install numpy')
-system('sudo pip install sympy')
-system('sudo pip install numexpr')
-system('sudo pip install matplotlib')
-system('sudo pip install redis')
-system('sudo pip install bokeh')
-system('sudo pip install ipython')
-system('sudo pip install panda')
-system('sudo pip install scipy')
-system('sudo pip install pytest')
-system('sudo pip install flake8')
-system('sudo pip install numpydoc')
-system('sudo pip install pygame')
+#pip  install
+pip_install setuptools
+pip_install numpy
+pip_install sympy
+pip_install numexpr
+pip_install matplotlib
+pip_install redis
+pip_install bokeh
+pip_install ipython
+pip_install panda
+pip_install scipy
+pip_install pytest
+pip_install flake8
+pip_install numpydoc
+pip_install pygame
 #
 # JR plot, CAD, mesh softwares
 #
-system('sudo apt-get -y install blender')
-system('sudo apt-get -y install freecad')
-system('sudo apt-get -y install mayavi2')
-system('sudo apt-get -y install meshlab')
+apt_install blender
+apt_install freecad
+apt_install mayavi2
+apt_install meshlab
 # Gnuplot (use qt backend)
-system('sudo apt-get -y install gnuplot')
-system('sudo apt-get -y install gnuplot-doc')
-system('sudo apt-get -y install gnuplot-qt')
-system('sudo apt-get -y install python-gnuplot')
-system('sudo apt-get -y install liblualib50-dev')
+apt_install gnuplot
+apt_install gnuplot-doc
+apt_install gnuplot-qt
+apt_install python-gnuplot
+apt_install liblualib50-dev
 
 
 # Plotting and visualization programs
-system('sudo apt-get -y install grace')
-system('sudo apt-get -y install dx')
-system('sudo apt-get -y install dx-doc')
-system('sudo apt-get -y install tcl-vtk')
-system('sudo apt-get -y install python-vtk')
-system('sudo apt-get -y install libvtk5-dev')
+apt_install grace
+apt_install dx
+apt_install dx-doc
+apt_install tcl-vtk
+apt_install python-vtk
+apt_install libvtk5-dev
 
 # JR pseudo-Matlab
-system('sudo apt-get -y install octave')
-system('sudo apt-get -y install octave-splines')
-system('sudo apt-get -y install octave-symbolic')
-system('sudo apt-get -y install octave-specfun')
-system('sudo apt-get -y install octave-optim')
-system('sudo apt-get -y install octave-odepkg')
+apt_install octave
+apt_install octave-splines
+apt_install octave-symbolic
+apt_install octave-specfun
+apt_install octave-optim
+apt_install octave-odepkg
 #octave-audio
 
 # Databases
-system('sudo apt-get -y install libsqlite3-dev')
+apt_install libsqlite3-dev
 
 # Drawing programs
-system('sudo apt-get -y install inkscape')
-system('sudo apt-get -y install xfig')
-system('sudo apt-get -y install xfig-doc')
-system('sudo apt-get -y install transfig')
-system('sudo apt-get -y install fig2ps')
+apt_install inkscape
+apt_install xfig
+apt_install xfig-doc
+apt_install transfig
+apt_install fig2ps
 
 # Image manipulation
-system('sudo apt-get -y install imagemagick')
-system('sudo apt-get -y install vlc')
-system('sudo apt-get -y install netpbm')
-system('sudo apt-get -y install mjpegtools')
-system('sudo apt-get -y install pdftk')
-system('sudo apt-get -y install giftrans')
-system('sudo apt-get -y install gv')
-system('sudo apt-get -y install evince')
-system('sudo apt-get -y install smpeg-plaympeg')
-system('sudo apt-get -y install mplayer')
-system('sudo apt-get -y install totem')
-system('sudo apt-get -y install ffmpeg')
-system('sudo apt-get -y install libav-tools')
+apt_install imagemagick
+apt_install vlc
+apt_install netpbm
+apt_install mjpegtools
+apt_install pdftk
+apt_install giftrans
+apt_install gv
+apt_install evince
+apt_install smpeg-plaympeg
+apt_install mplayer
+apt_install totem
+apt_install ffmpeg
+apt_install libav-tools
 
 
 # Support for French
-system('sudo apt-get -y install language-pack-fr-base')
-system('sudo apt-get -y install language-pack-fr')
+apt_install language-pack-fr-base
+apt_install language-pack-fr
 
 # Download source code and install in srclib subdirectory
 
@@ -217,3 +200,47 @@ system('sudo apt-get -y install language-pack-fr')
 
 # Install OpenFOAM code in OpenFOAM
 # cd
+if [ ! -d OpenFOAM ]; then mkdir OpenFOAM; fi
+
+cd OpenFOAM
+wget "http://download.openfoam.org/source/4-1" -O OpenFOAM-4.1.tgz
+wget "http://download.openfoam.org/third-party/4-1" -O ThirdParty-4.1.tgz
+tar -xzf OpenFOAM-4.1.tgz
+tar -xzf ThirdParty-4.1.tgz
+mv OpenFOAM-4.x-version-4.1 OpenFOAM-4.1
+mv ThirdParty-4.x-version-4.1 ThirdParty-4.1
+ln -s /usr/bin/mpicc.openmpi OpenFOAM-4.1/bin/mpicc
+ln -s /usr/bin/mpirun.openmpi OpenFOAM-4.1/bin/mpirun
+source $HOME/OpenFOAM/OpenFOAM-4.1/etc/bashrc WM_LABEL_SIZE=64 FOAMY_HEX_MESH=yes
+echo "alias of41='source \$HOME/OpenFOAM/OpenFOAM-4.1/etc/bashrc $FOAM_SETTINGS'" >> $HOME/.bashrc
+cd $WM_THIRD_PARTY_DIR
+export QT_SELECT=qt4
+./makeParaView -python -mpi -python-lib /usr/lib/x86_64-linux-gnu/libpython2.7.so.1.0 > log.makePV 2>&1
+wmRefresh
+cd $WM_PROJECT_DIR
+export QT_SELECT=qt4
+./Allwmake > log.make 2>&1
+cd ..
+rm OpenFOAM-4.1.tgz
+rm ThirdParty-4.1.tgz
+
+# Clean up
+sudo mv -f src/* srclib
+sudo rm -rf src build
+sudo find srclib -name build -exec rm -rf {} \;
+cd
+sudo rm -rf .matplotlib
+mkdir .matplotlib
+sudo apt-get clean
+sudo apt-get autoclean
+sudo apt-get -y autoremove
+
+
+
+
+
+
+
+
+echo "Everything is successfully installed!"
+
