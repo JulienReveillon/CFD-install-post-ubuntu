30fd45d8ea0c57891d06fac28a21d39d12734851
Julien Reveillon <Julien.Reveillon@coria.fr>
2022-03-25 16:44:54 +0100

    change wallpaper position

--- CFD-install-baseCFD.sh
+++ CFD-install-baseCFD.sh
@@ -199,6 +199,6 @@ sudo sed -i 's/enabled=1/enabled=0/' /etc/default/apport
 sudo systemctl disable apport.service
 #
 # Wallpaper I use
-wget https://www.dropbox.com/s/1qkimjutqdt0lrb/black-waves-3840x2160-4k.jpg?dl=0 -O /tmp/blackwaves.jpg
-gsettings set org.gnome.desktop.background picture-uri file:////tmp/blackwaves.jpg
+wget https://www.dropbox.com/s/1qkimjutqdt0lrb/black-waves-3840x2160-4k.jpg?dl=0 -O $HOME/blackwaves.jpg
+gsettings set org.gnome.desktop.background picture-uri file:///$HOME/blackwaves.jpg
 echo "Done !"
